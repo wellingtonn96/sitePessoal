1b10a1ec22da201da3699ab3bedf6846dae9c6cb
Wellington Santos <weltossousa@gmail.com>
2020-06-16 17:04:35 -0300

    criando responsividade do site

--- src/src/pages/Home/index.tsx
+++ src/src/pages/Home/index.tsx
@@ -1,59 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
+import { FaBars } from "react-icons/fa";
 
 import "./style.css";
 
 export default function App() {
   return (
     <>
-      <header className="header">
-        <h1>Wellington</h1>
-        <nav>
-          <ul>
+      <div className="container">
+        <header className="header">
+          <div className="header-menu">
+            <h1>Wellington</h1>
+            <div className="menu-icon">
+              <FaBars size={30} onClick={() => !true} />
+            </div>
+          </div>
+
+          <nav>
+            <ul>
+              <li>
+                <Link to="/projects">projects</Link>
+              </li>
+              <li>
+                <Link to="/contanct">contact</Link>
+              </li>
+              <li className="nav-social-media">
+                <ul>
+                  <li>
+                    <Link to="#">
+                      <FiGithub size={20} />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#">
+                      <FiLinkedin size={20} />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#">
+                      <FiTwitter size={20} />
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="main">
+          <div className="content">
+            <p style={{ marginRight: 150 }}>i am</p>
+            <h1>Wellington</h1>
+            <p style={{ marginLeft: 150 }}>
+              a junior <br /> web developer
+            </p>
+          </div>
+          <ul className="social-media">
             <li>
-              <a href="#">projects</a>
+              <a href="#">
+                <FiGithub size={20} />
+              </a>
             </li>
             <li>
-              <a href="#">contact</a>
+              <a href="#">
+                <FiLinkedin size={20} />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FiTwitter size={20} />
+              </a>
             </li>
           </ul>
-        </nav>
-      </header>
-      <main className="main">
-        <div className="content">
-          <p>i am</p>
-          <p
-            style={{
-              fontWeight: 1000,
-              fontSize: 100,
-              textTransform: "uppercase",
-            }}
-          >
-            Wellington
-          </p>
-          <p>a junior</p>
-          <p>web developer</p>
-        </div>
-        <div className="social-media">
-          <ul>
-            <li>
-              <a href="#">
-                <FiGithub />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FiLinkedin />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FiTwitter />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   );
 }
--- src/src/pages/Projects/index.tsx
+++ src/src/pages/Projects/index.tsx
@@ -1,5 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
 
-export default function App() {
-  return <h1>Projects Page</h1>;
+import "./style.css";
+
+export default function Projects() {
+  return (
+    <>
+      <div className="contanier">
+        <header className="header">
+          <h1>Wellington</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">home</Link>
+              </li>
+              <li>
+                <Link to="/projects">projects</Link>
+              </li>
+              <li>
+                <Link to="/contact">contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="main">
+          <div className="content">
+            <p style={{ marginLeft: 150 }}>i am</p>
+            <h1>Wellington</h1>
+            <p
+              style={{
+                marginLeft: 500,
+                textAlign: "left",
+              }}
+            >
+              a junior <br /> web developer
+            </p>
+          </div>
+          <div className="social-media">
+            <ul>
+              <li>
+                <a href="#">
+                  <FiGithub size={20} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FiLinkedin size={20} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FiTwitter size={20} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
